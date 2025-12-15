c4e848f339fae69c680382dde0113f7ce33cb6e4
Sara Salzano <sarasalzano95@gmail.com>
2025-12-15 13:23:36 +0100

    esegui gestione errori

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 async function getChefBirthday(id) {
- 
+  try {
     const responseRecipe = await fetch(`https://dummyjson.com/recipes/${id}`);
     const recipe = await responseRecipe.json();
-
     const userId = recipe.userId;
+
     const responseUser = await fetch(`https://dummyjson.com/users/${userId}`);
     const infoUser = await responseUser.json();
     const birthday = infoUser.birthDate;
     return birthday;
+  } catch (errore) {
+    console.error("Errore", errore.message);
+  }
 }
 
 getChefBirthday(1)
