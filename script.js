cdb5fa104ea5115d4443e54123a94f5f62d7bb7c
Sara Salzano <sarasalzano95@gmail.com>
2025-12-15 13:22:50 +0100

    esegui stampa e errore

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ async function getChefBirthday(id) {
  
     const responseRecipe = await fetch(`https://dummyjson.com/recipes/${id}`);
     const recipe = await responseRecipe.json();
-    
+
     const userId = recipe.userId;
     const responseUser = await fetch(`https://dummyjson.com/users/${userId}`);
     const infoUser = await responseUser.json();
@@ -10,4 +10,6 @@ async function getChefBirthday(id) {
     return birthday;
 }
 
-
+getChefBirthday(1)
+  .then((birthday) => console.log("Data di nascita dello chef:", birthday))
+  .catch((error) => console.error("Errore:", error.message));
